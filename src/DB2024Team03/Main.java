ddbe93d2c8b1d3d14a8c9ade7eff9dcb787c5fcb
nomellc <127232686+nomellc@users.noreply.github.com>
2024-05-27 21:25:40 +0900

    feat: 마이페이지

--- src/DB2024Team03/Main.java
+++ src/DB2024Team03/Main.java
@@ -28,11 +28,41 @@ public class Main {
 				//로그인 성공
 				if(member != null) {
 					while(true) {
+						System.out.println(member.getName() + "님 환영합니다!");
 						System.out.println("==================자취생을 위한 밀키트 쇼핑몰==================");
 						System.out.println("[1]상품목록 [2]마이페이지 [3]장바구니 보러가기 [4]북마크 >> ");
 						
 						int mainselect = sc.nextInt();
 						////이 밑으로 계속 연결하기!!!
+						// 1. 상품목록
+						if (mainselect == 1) {
+
+						}
+
+						// 2. 마이페이지
+						else if (mainselect == 2) {
+							System.out.println("========================마이페이지=========================");
+							System.out.println(member.getName());
+							System.out.println(member.getAddress());
+							System.out.print("[1]북마크 [2]장바구니 [3]작성했던 리뷰 목록 [4]주문내역 [5]메인페이지로 이동 >> ");
+
+							int myselect = sc.nextInt();
+
+							// 1. 북마크
+							if (myselect == 1) {
+
+							}
+
+							// 2. 장바구니
+							else if (myselect == 2) {
+
+							}
+
+							// 3. 작성했던 리뷰 목록
+							else if (myselect == 3) {
+
+							}
+						}
 					}
 				}
 			}
