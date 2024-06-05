9eb5013d240217eee51116a06d4cc5146014d766
JEES23 <76464398+JEES23@users.noreply.github.com>
2024-06-05 14:22:32 +0900

    util 파일 생성

--- src/DB2024Team03/UtilController.java
+++ src/DB2024Team03/UtilController.java
@@ -1,5 +1,31 @@
 package DB2024Team03;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UtilController {
-    //empty
+    public static int checkBasket(int id){
+        String checkBasket = "SELECT COUNT(*) FROM DB2024_BASKET WHERE member_id=?";
+
+        try (// DB 연결을 위한 정보를 설정
+             Connection conn = DBconnect.getConnection();
+             PreparedStatement statement = conn.prepareStatement(checkBasket);
+        ){
+
+            statement.setInt(1, id);
+
+            try(ResultSet resultSet = statement.executeQuery()){
+                // 장바구니 아이템 개수 받아오기
+                if (resultSet.next()){
+                    return resultSet.getInt(1);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return 0;
+    }
 }
