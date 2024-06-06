cb4d65b55d130ddc08b8b47b3f8dbad339fde305
Seoa-S <0325sandy@ewhain.net>
2024-06-06 17:16:59 +0900

    .

--- src/DB2024Team03/UtilController.java
+++ src/DB2024Team03/UtilController.java
@@ -7,28 +7,28 @@ import java.sql.SQLException;
 
 public class UtilController {
 
-    //테이블 안에 있는 상품의 개수 세기
-    public static int checkItemNum(int memberId, String DBName, Connection conn){
-        String checkBasket = "SELECT COUNT(*) FROM " +DBName+ " WHERE member_id=?";
+    //테이블 안에 있는 튜플의 개수 세는 함수
+    public static int checkItemNum(int memberId, String DBName, Connection conn){ //memberId, 데이터베이스 테이블 이름, connection을 입력받는다.
+        String checkItemNum = "SELECT COUNT(*) FROM " +DBName+ " WHERE member_id=?"; // DBName 테이블에서 member_id가 일치하는 행의 개수를 세는 SQL 쿼리를 문자열로 생성
 
-        try (// DB 연결을 위한 정보를 설정
-             PreparedStatement statement = conn.prepareStatement(checkBasket);
+        try (
+             PreparedStatement statement = conn.prepareStatement(checkItemNum); // DB와의 연결을 위한 PreparedStatement를 생성
         ){
 
-            statement.setInt(1, memberId);
+            statement.setInt(1, memberId); // SQL 쿼리의 첫 번째 매개변수'?'에 memberId를 설정
 
-
-            try(ResultSet resultSet = statement.executeQuery()){
+            try(ResultSet resultSet = statement.executeQuery()){ // SQL 쿼리를 실행하고, 그 결과를 ResultSet 객체로 받아온다.
                 // 상품 개수 받아오기
-                if (resultSet.next()){
-                    return resultSet.getInt(1);
+                if (resultSet.next()){ // ResultSet에 다음 행이 존재한다면(즉, 쿼리의 결과가 존재한다면)
+                    return resultSet.getInt(1); // 첫 번째 열(여기서는 COUNT(*)의 결과)의 값을 int로 받아와서 반환합니다.
                 }
             }
         } catch (SQLException e) {
+            // SQL 관련 예외가 발생하면, 런타임 예외로 전환하여 던집니다.
             throw new RuntimeException(e);
         }
 
-        return 0;
+        return 0; // SQL 쿼리의 결과가 없거나 예외가 발생했을 때, 0을 반환합니다.
     }
 
     public static boolean checkIdExist(int itemId, int memberId, String tableName, Connection conn) {
